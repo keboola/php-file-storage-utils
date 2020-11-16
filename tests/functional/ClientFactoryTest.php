9dce4a096ebd326ea472a8e39ea7b37259d52afe
Martin Zajic <zajca@zajca.cz>
2020-11-16 09:42:06 +0100

    use different container name creation

--- tests/functional/ClientFactoryTest.php
+++ tests/functional/ClientFactoryTest.php
@@ -10,20 +10,31 @@ class ClientFactoryTest extends BaseFunctionalTestCase
 {
     private const CONTAINER = 'ping';
 
+    /** @var string|null */
+    private static $containerName = null;
+
     protected function setUp(): void
     {
         parent::setUp();
         try {
-            $this->client->deleteContainer(self::CONTAINER);
+            $this->client->deleteContainer(self::getContainerName());
         } catch (\Throwable $e) {
             // ignore
         }
     }
 
+    private static function getContainerName(): string
+    {
+        if (self::$containerName === null) {
+            self::$containerName = self::CONTAINER . md5(uniqid('', true));
+        }
+        return self::$containerName;
+    }
+
     public function testClient(): void
     {
-        $this->client->createContainer(self::CONTAINER);
-        $this->client->deleteContainer(self::CONTAINER);
+        $this->client->createContainer(self::getContainerName());
+        $this->client->deleteContainer(self::getContainerName());
 
         $this->expectNotToPerformAssertions();
     }
