2be072e1c30047630439bcb80777466a7a4d7ef7
Martin Zajic <zajca@zajca.cz>
2020-11-16 13:22:19 +0100

    fix and simplify echo logger

--- tests/common/EchoLogger.php
+++ tests/common/EchoLogger.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Keboola\FileStorage\Tests\Common;
 
-use Psr\Http\Message\RequestInterface;
-use Psr\Http\Message\ResponseInterface;
 use Psr\Log\AbstractLogger;
 
 class EchoLogger extends AbstractLogger
@@ -14,31 +12,10 @@ class EchoLogger extends AbstractLogger
     public function log($level, $message, array $context = [])
     {
         // phpcs:enable
-        echo $message;
+        echo sprintf('[%s] %s', $level, $message);
         echo PHP_EOL;
-        if (array_key_exists('request', $context)) {
-            /** @var RequestInterface $request */
-            $request = $context['request'];
-            echo $request->getBody();
-            echo PHP_EOL;
-        }
-
-        if (array_key_exists('response', $context)) {
-            /** @var ResponseInterface $request */
-            $request = $context['response'];
-            echo $request->getBody();
-            echo PHP_EOL;
-        }
-
-        if (array_key_exists('reason', $context)) {
-            var_export($context['reason']);
-            echo PHP_EOL;
-        }
-
-        if (array_key_exists('options', $context)) {
-            var_export($context['options']);
-            echo PHP_EOL;
-        }
+        var_export($context);
+        echo PHP_EOL;
 
         ob_clean();
         ob_flush();
